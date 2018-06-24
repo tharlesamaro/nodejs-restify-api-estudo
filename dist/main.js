596d06f0696cc5d30c125ceb280e383c86b9271a
Tharles Amaro <tharlesamaro@gmail.com>
2018-06-23 22:14:56 -0300

    primeiro exemplo com restify

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,16 @@
-console.log('main script 2...');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'api-restify',
+    version: '1.0.0'
+});
+server.get('/hello', (req, resp, next) => {
+    resp.json({
+        message: 'Hello World'
+    });
+    return next();
+});
+server.listen(3000, () => {
+    console.log('API is running on http://localhost:3000');
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log('main script 2...')
+import * as restify from "restify"
+
+const server = restify.createServer({
+  name: 'api-restify',
+  version: '1.0.0'
+})
+
+server.get('/hello', (req, resp, next) => {
+  resp.json({
+    message: 'Hello World'
+  })
+  return next()
+})
+
+server.listen(3000, () => {
+  console.log('API is running on http://localhost:3000')
+})
