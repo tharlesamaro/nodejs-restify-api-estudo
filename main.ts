dc733de4fd491d4c8f2558595e056613f49f5f9c
Tharles Amaro <tharlesamaro@gmail.com>
2018-06-23 23:20:19 -0300

    estudando objetos request, response e netxt

--- main.ts
+++ main.ts
@@ -5,12 +5,38 @@ const server = restify.createServer({
   version: '1.0.0'
 })
 
-server.get('/hello', (req, resp, next) => {
-  resp.json({
-    message: 'Hello World'
-  })
-  return next()
-})
+server.use(restify.plugins.queryParser())
+
+server.get('/info', [
+  (req, resp, next) => {
+    if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
+      // resp.status(400)
+      // resp.json({
+      //   message: 'Please, update your browser'
+      // })
+      let error: any = new Error()
+      error.statusCode = 400
+      error.message = 'Please, update your browser'
+      return next(error)
+      //return next(false)
+    }
+    return next()
+  },
+  (req, resp, next) => {
+    //resp.contentType = 'application/json'
+    //resp.status(400)
+    //resp.setHeader('Content-Type', 'application/json')
+    //resp.send({message: 'Hello World'})
+    resp.json({
+      browser: req.userAgent(),
+      method: req.method,
+      url: req.href(), //req.url
+      path: req.path(),
+      query: req.query
+    })
+    return next()
+  }
+])
 
 server.listen(3000, () => {
   console.log('API is running on http://localhost:3000')
