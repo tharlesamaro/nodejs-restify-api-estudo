cd6d2488dcf1a7d8a407f0751979841549073c52
Tharles Amaro <tharlesamaro@gmail.com>
2018-06-24 19:44:55 -0300

    substituindo um documento via PUT

--- dist/users/users.router.js
+++ dist/users/users.router.js
@@ -12,7 +12,8 @@ class UsersRouter extends router_1.Router {
             });
         });
         application.get('/users/:id', (req, resp, next) => {
-            users_model_1.User.findById(req.params.id).then(user => {
+            users_model_1.User.findById(req.params.id)
+                .then(user => {
                 if (user) {
                     resp.json(user);
                     return next();
@@ -23,8 +24,28 @@ class UsersRouter extends router_1.Router {
         });
         application.post('/users', (req, resp, next) => {
             let user = new users_model_1.User(req.body);
-            user.save().then(user => {
+            user.save()
+                .then(user => {
                 user.password = undefined;
+                resp.json(user);
+                return next();
+            });
+        });
+        application.put('/users/:id', (req, resp, next) => {
+            const options = {
+                overwrite: true
+            };
+            users_model_1.User.update({ _id: req.params.id }, req.body, options)
+                .exec()
+                .then(result => {
+                if (result.n) {
+                    return users_model_1.User.findById(req.params.id);
+                }
+                else {
+                    resp.send(404);
+                }
+            })
+                .then(user => {
                 resp.json(user);
                 return next();
             });
--- users/users.router.ts
+++ users/users.router.ts
@@ -14,24 +14,46 @@ class UsersRouter extends Router {
     })
 
     application.get('/users/:id', (req, resp, next) => {
-      User.findById(req.params.id).then(user => {
-        if (user) {
-          resp.json(user)
+      User.findById(req.params.id)
+        .then(user => {
+          if (user) {
+            resp.json(user)
+            return next()
+          }
+          resp.send(404)
           return next()
-        }
-        resp.send(404)
-        return next()
-      })
+        })
     })
 
     application.post('/users', (req, resp, next) => {
       let user = new User(req.body)
-      user.save().then(user => {
-        user.password = undefined
-        resp.json(user)
-        return next()
-      })
+      user.save()
+        .then(user => {
+          user.password = undefined
+          resp.json(user)
+          return next()
+        })
     })
+
+    application.put('/users/:id', (req, resp, next) => {
+      const options = {
+        overwrite: true
+      }
+      User.update({_id: req.params.id}, req.body, options)
+        .exec()
+        .then(result => {
+          if (result.n) {
+            return User.findById(req.params.id)
+          } else {
+            resp.send(404)
+          }
+        })
+        .then(user => {
+          resp.json(user)
+          return next()
+        })
+    })
+
   }
 }
 
